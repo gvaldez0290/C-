457d811d914da64c87e1875648665bdc8cd080ea
Gerardo "Jerry" Valdez <gvaldez0290@gmail.com>
2023-11-10 14:44:49 -0500

    Update helloworld.cs

--- helloworld.cs
+++ helloworld.cs
@@ -1,6 +1,10 @@
+// lesson 1: https://learn.microsoft.com/en-us/training/modules/csharp-write-first/
+
 console.WriteLine("Hello World!");
 
 //WriteLine appends a newline after output
 Console.WriteLine("This is the first line.");
-Console.WriteLine("This is the second line.");
+Console.Write("This is ");
+Console.Write("the second ");
+Console.Write("line.");
 
