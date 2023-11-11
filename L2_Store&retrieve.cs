53ca255c9ebf5be0317f7bba861b395691ad3aad
Gerardo "Jerry" Valdez <gvaldez0290@gmail.com>
2023-11-11 16:07:25 -0500

    Create L2_Store&retrieve.cs

---
+++ L2_Store&retrieve.cs
@@ -0,0 +1,26 @@
+//https://learn.microsoft.com/en-us/training/modules/csharp-literals-variables/
+
+
+// Char literals. notice the signle quotes for the char. whereas the strings have double
+Console.WriteLine('b');
+
+// Integer literals 
+Console.WriteLine(123);
+
+// Floats: append the letter F adter the decimal number 
+Console.WriteLine(0.25F);
+
+// Doubles: To create a double literal, just enter a decimal number.
+Console.WriteLine(2.625);
+
+// decimal literal, append the letter m after the number. In this context, the m is called a literal suffix. 
+Console.WriteLine(12.39816m);
+
+//booleans
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+
+
+
+
